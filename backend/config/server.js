7fb06c05342bf3ca0d6a276467dcd52eed632474
ivoaguirra <ivo.aguirra@gmail.com>
2026-01-15 15:37:52 -0300

    Change export syntax from ES6 to CommonJS

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,2 +1,11 @@
-export default ({ env })
+// config/server.js
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", ""),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
+
 
